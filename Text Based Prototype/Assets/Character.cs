64fe4ac91e7577705a170263bde30ff062fcc895
Satrio Triatmoko <tio.triatmoko@outlook.com>
2018-01-12 22:50:04 +0800

    init commit

---
+++ Text Based Prototype/Assets/Character.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Character {
+	string name;
+	// determine the number of image later 
+	Sprite[] spriteSheet; 
+
+	public Sprite getDefaultSprite(){
+		return spriteSheet [0];
+	}
+
+	public Sprite getHappySprite() {
+		return spriteSheet [1];
+	}
+
+	public Sprite getSadSprite() {
+		return spriteSheet [2];
+	}
+}
---
+++ Text Based Prototype/Assets/TrialScript.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TrialScript : MonoBehaviour {
+	[SerializeField]
+	string sampleText; 
+	[SerializeField]
+	Text textContainer; 
+	[SerializeField]
+	float additionCharacterCoolDown;
+
+	char[] currentText; 
+	char[] shownText;
+	float waitingTime;
+	bool hasFinishedShowingText; 
+
+	// Use this for initialization
+	void Start () {
+		textContainer.text = string.Empty;	
+		currentText = sampleText.ToCharArray(); 
+		shownText = new char[currentText.Length];
+		waitingTime = 0.0f; 
+		hasFinishedShowingText = false; 
+	}
+	
+	// Update is called once per frame
+
+	void Update () {
+		if (hasFinishedShowingText) {
+			return; 
+		}
+		if (waitingTime < additionCharacterCoolDown) {
+			waitingTime += Time.deltaTime;
+			return; 
+		} else {
+			updateShownText(); 
+		}
+
+
+	}
+
+	int currentCharacterPointer = 0; 
+	void updateShownText(){
+
+			if (currentCharacterPointer == currentText.Length) {
+				hasFinishedShowingText = true; 
+				return;
+			}
+
+			shownText [currentCharacterPointer] = currentText [currentCharacterPointer++];
+			textContainer.text = new string (shownText);
+			waitingTime = 0.0f; 
+	}
+
+}
