60c7c9538fc22698ba69b79f6f282114dc72b6ba
Satrio Triatmoko <tio.triatmoko@outlook.com>
2018-01-13 19:38:31 +0800

    Update unity version

---
+++ Text Based Prototype/Assets/Script/Character/You.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// this script should contain the player's progression and their current stats on the game(affection, etc)
+public class You : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
