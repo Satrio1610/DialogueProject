a179523d9518be06d1abd0de07fb1e7a121803d5
Satrio Triatmoko <tio.triatmoko@outlook.com>
2018-01-14 03:19:11 +0800

    move .gitignore and update dialogue script to include accessor. Delete Library folder

--- Text Based Prototype/Assets/Script/DialoguesSystem/Dialogue.cs
+++ Text Based Prototype/Assets/Script/DialoguesSystem/Dialogue.cs
@@ -11,9 +11,26 @@ public class Dialogue : Node {
 	[SerializeField]
 	private Character character;
 
+	[SerializeField]
+	private Character.CHARACTER_EXPRESSION expression; 
+
 	public Dialogue(int id) : base(id){
 		
 		this.NodeType = NODE_TYPE.DIALOGUE;
+		this.expression = Character.CHARACTER_EXPRESSION.DEFAULT;
 		ListOfDialogues = new List<string> ();
+	}
+
+	public void setCharacter(Character character){
+		this.character = character;
+	}
+
+	public void setCharacterExpression(Character.CHARACTER_EXPRESSION expression){
+		this.expression = expression; 
+
+	}
+
+	public void setListOfDialogues(List<string> dialogues) {
+		this.ListOfDialogues = new List<string> (dialogues);
 	}
 }
